eec38c01673d1bc833f54e6a6c474d06e154c2ec
Dmitri Zawadski <dzdimati@gmail.com>
2019-09-02 17:21:35 +0300

    add models

--- officemanager/department/models/models.py
+++ officemanager/department/models/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class departments(models.Model):
+    name = models.CharField(max_length=200)
+
+class employees(models.Model):
+    department = models.ForeignKey("departments", on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    d_of_b = models.DateField(auto_now=False, auto_now_add=False)
+    salary = models.DecimalField( max_digits=9, decimal_places=2)
