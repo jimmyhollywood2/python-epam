c469ab785575b58a3770934da52c1a4f5e5da4d5
Dmitri Zavadski <dzdimati@gmail.com>
2019-09-16 16:38:27 +0300

    add tests

--- officemanager/department/tests/tests.py
+++ officemanager/department/tests/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from department.models import Department, Employee
 from department.service import service
+import json
 
 class StringReprTestCase(TestCase):
     
@@ -13,7 +14,7 @@ class StringReprTestCase(TestCase):
         dep.save()
         self.assertEqual(str(dep), dep.name)
     
-    def test_is_save(self):
+    def test_str(self):
         """
         Testing string representation of the model
         """
@@ -27,7 +28,7 @@ class StringReprTestCase(TestCase):
         emp.save()
         self.assertEqual(str(emp), "Dmitri Zavadski")
 
-class RESTTestCase(APITestCase):
+class ApiList(APITestCase):
 
     def test_department_list(self):
         resp = self.client.get('/api/department/')
@@ -57,99 +58,183 @@ class RESTTestCase(APITestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp_record['first_name'], asserted.first_name)
 
-    def test_create_department(self):
+class ApiCreateDepartment(APITestCase):
+
+    def setUp(self):
+        self.valid_data = {
+            'name': 'IT department',
+        }
+        self.invalid_data = {
+            'name': '',
+        }
+    def test_create_valid_department(self):
         """
         Create new department
         """
-        data = {
-            'name': "IT department",
+        resp = self.client.post('/api/department/', self.valid_data, format='json')
+        self.assertEqual(self.valid_data['name'], resp.json()['name'])
+    
+    def test_create_invalid_department(self):
+        """
+        Create department with invalid data
+        """
+        resp = self.client.post('/api/department/', self.invalid_data, format='json')
+        self.assertEqual(resp.status_code, 400)
+
+class ApiCreateEmployee(APITestCase):
+
+    def setUp(self):
+        self.valid_data = {
+            'department': Department.objects.get(pk=1).pk,
+            'first_name': 'Bruce',
+            'last_name': 'Willis',
+            'd_of_b': '1955-03-19',
+            'salary': 15000.00,
         }
-        resp = self.client.post('/api/department/', data, format='json')
-        asserted = Department.objects.get(pk=resp.json()['id'])
-        self.assertEqual(resp.status_code, 201)
-        self.assertEqual(data['name'], asserted.name)
+        self.invalid_data = {
+            'department': Department.objects.get(pk=1).pk,
+            'first_name': '',
+            'last_name': '',
+            'd_of_b': '1955-03-19',
+            'salary': 15000.00,
+        }
 
     def test_create_employee(self):
         """
         Create new employee
         """
-        data = {
-            'department': Department.objects.get(pk=1).pk,
+        resp = self.client.post('/api/employee/', self.valid_data, format='json')
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(self.valid_data['last_name'], resp.json()['last_name'])
+
+    def test_create_invalid_employee(self):
+        """
+        Create new employee with invalid data
+        """
+        resp = self.client.post('/api/employee/', self.invalid_data, format='json')
+        self.assertEqual(resp.status_code, 400)
+
+class PutDepartment(TestCase):
+
+    def setUp(self):
+        self.valid_data = {
+            'name': 'New department',
+        }
+        self.invalid_data = {
+            'name': '',
+        }
+        Department.objects.create(name="New department")
+        self.dep_id = Department.objects.get(name="New department").pk
+
+    def test_put_department(self):
+        """
+        Put valid data
+        """
+        resp = self.client.put(
+            '/api/department/{}/'.format(self.dep_id),
+            self.valid_data,
+            content_type='application/json'
+        )
+        self.assertEqual(self.valid_data['name'], resp.json()['name'])
+    
+    def test_put_invalid_department(self):
+        """
+        Put invalid data 
+        """
+        resp = self.client.put(
+            '/api/department/{}/'.format(self.dep_id),
+            self.invalid_data,
+            content_type='application/json'
+        )
+        self.assertEqual(resp.status_code, 400)
+
+class PutEmployee(TestCase):
+    
+    def setUp(self):
+        self.valid_data = {
+            'department': Department.objects.all()[1].pk,
             'first_name': 'Bruce',
             'last_name': 'Willis',
             'd_of_b': '1955-03-19',
-            'salary': 15000.00
+            'salary': 15000.00,
         }
-        resp = self.client.post('/api/employee/', data, format='json')
-        asserted = Employee.objects.get(pk=resp.json()['id'])
-        self.assertEqual(resp.status_code, 201)
-        self.assertEqual(data['first_name'], asserted.first_name)
-        self.assertEqual(data['last_name'], asserted.last_name)
-    
-    def test_put_department(self):
-        """
-        Edit record of department table
-        """
-        data = {
-            'name': 'New name',
+        self.invalid_data = {
+            'department': Department.objects.all()[1].pk,
+            'first_name': '',
+            'last_name': '',
+            'd_of_b': '1955-03-19',
+            'salary': 15000.00,
         }
-        resp = self.client.put('/api/department/1/', data, format='json')
-        asserted = Department.objects.get(pk=resp.json()['id'])
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(data['name'], asserted.name)
+        Employee.objects.create(
+            department=Department.objects.all()[1],
+            first_name='Test',
+            last_name='Test',
+            d_of_b='1900-01-01',
+            salary=1000.00,
+        )
+        self.emp_id = Employee.objects.get(first_name="Test").pk
     
     def test_put_employee(self):
         """
         Edit record of employee table
         """
-        data = {
-            'department': Department.objects.all()[1].pk,
-            'first_name': 'Test',
-            'last_name': 'Test',
-            'd_of_b': '1955-03-19',
-            'salary': 15000.00
-        }
-        resp = self.client.put('/api/employee/1/', data, format='json')
-        asserted = Employee.objects.get(pk=resp.json()['id'])
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(data['last_name'], asserted.last_name)
-    
+        resp = self.client.put(
+            '/api/employee/{}/'.format(self.emp_id),
+            data=json.dumps(self.valid_data),
+            content_type='application/json')
+        self.assertEqual(self.valid_data['last_name'], resp.json()['last_name'])
+
+    def test_put_invalid_employee(self):
+        """
+        Edit record of employee table
+        """
+        resp = self.client.put(
+            '/api/employee/{}/'.format(self.emp_id),
+            data=json.dumps(self.invalid_data),
+            content_type='application/json')
+        self.assertEqual(400, resp.status_code)
+
+class DeleteDepartment(TestCase):
+
+    def setUp(self):
+        Department.objects.create(name="ToDelete")
+        self.dep_id = Department.objects.get(name='ToDelete').pk
+        self.valid_url = '/api/department/{}/'.format(self.dep_id)
+        self.invalid_url = '/api/department/{}/'.format(214124)
+
     def test_delete_department(self):
-        """
-        Delete record from department table
-        """
-        Department.objects.create(name="Test")
-        added_id = Department.objects.get(name="Test").pk
-        self.client.delete('/api/department/{}/'.format(added_id))
-        resp = self.client.get('/api/department/{}/'.format(added_id))
-        self.assertEqual('Not found.', resp.json()['detail'])
+        resp = self.client.delete(self.valid_url)
+        self.assertEqual(204, resp.status_code)
+
+    def test_delete_invalid_url_department(self):
+        resp = self.client.delete(self.invalid_url)
+        self.assertEqual(404, resp.status_code)
+
+class DeleteEmployee(TestCase):
+
+    def setUp(self):
+        Employee.objects.create(
+            department=Department.objects.all()[1],
+            first_name='ToDelete',
+            last_name='ToDelete',
+            d_of_b='1900-01-01',
+            salary=1000.00,
+        )
+        self.emp_id = Employee.objects.get(first_name="ToDelete").pk
+        self.valid_url = '/api/employee/{}/'.format(self.emp_id)
+        self.invalid_url = '/api/employee/{}/'.format(214124)
 
     def test_delete_employee(self):
-        """
-        Delete record from employee table
-        """
-        data = {
-            'department': Department.objects.all()[4],
-            'first_name': 'Test',
-            'last_name': 'Test',
-            'd_of_b': '1955-03-19',
-            'salary': 15000.00
-        }
-        cr = Employee.objects.create(
-            department=data['department'],
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            d_of_b=data['d_of_b'],
-            salary=data['salary'],
-        )
-        added_id = max([i.id for i in Employee.objects.all()])
-        self.client.delete('/api/employee/{}/'.format(added_id))
-        resp = self.client.get('/api/employee/{}/'.format(added_id))
-        self.assertEqual('Not found.', resp.json()['detail'])
+        resp = self.client.delete(self.valid_url)
+        self.assertEqual(204, resp.status_code)
+    
+    def test_delete_invalid_url_employee(self):
+        resp = self.client.delete(self.invalid_url)
+        self.assertEqual(404, resp.status_code)
 
-class ServiceTestCase(TestCase):
-    def test_get_department(self):
-        actual = [i['name'] for i in service.get_departments()]
+class ServiceGetDepartments(TestCase):
+    def test_get(self):
+        resp = [i['name'] for i in service.get_departments()]
         asserted = [i.name for i in Department.objects.all()]
-        self.assertEqual(len(actual), len(asserted))
+        self.assertEqual(sorted(resp), sorted(asserted))
         
